5a8f3d5450f9c73d07c531453ec4becd61961ab9
Black0777 <87493630+Black0777@users.noreply.github.com>
2022-08-17 13:21:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ print(pyfiglet.figlet_format(" marshal", font="digital"))
 print(pyfiglet.figlet_format("CoHost Spam", font="small"))
 f='━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
 client = samino.Client()
-e="jtrmvft7h@1secmail.com"
-p="786786"
+e="k4puzx75@1secmail.org"
+p="223344"
 client.login(e,p)
 print("\nLogin Successfully")
 print(f)
 #keep_alive()
 client.headers
-HostChatLink="http://aminoapps.com/p/3astkgt"
+HostChatLink="http://aminoapps.com/p/uuszxa"
 config_chat = client.get_from_link(HostChatLink).json #You have  host on chat
 thread_id = config_chat["linkInfo"]["objectId"]
 ndc_id = config_chat["linkInfo"]["ndcId"]
 local = samino.Local(comId = ndc_id)
-targetlink="http://aminoapps.com/p/mwxnxj"
+targetlink="http://aminoapps.com/p/5difbyz"
 remove = client.get_from_link(str(targetlink)).objectId
 while True:
 	print("boomm.......")
